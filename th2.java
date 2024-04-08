9d5b3a8ee903f77d9d7f53c7c9733303ce07bd42
kieutandung <145318268+kieutandung@users.noreply.github.com>
2024-04-08 16:00:56 +0700

    Add files via upload

--- th2.java
+++ th2.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class th2 {
     public static void main(String[] args) {
-        float chieudai;
-        float chieucao;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("nhập chiều dài");
-        chieudai= scanner.nextFloat();
-        System.out.println("nhập chiều cao");
-        chieucao= scanner.nextFloat();
-        float area = chieucao * chieudai;
-        System.out.println("area là:" + area);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số lượng tiền cho vay: ");
+        double tienVay = sc.nextDouble();
+
+        System.out.print("Nhập tỉ lệ lãi suất theo tháng (%): ");
+        double laiSuat = sc.nextDouble();
+
+        System.out.print("Nhập số tháng cho vay: ");
+        int soThang = sc.nextInt();
+
+        double tongTien = tinhTienLai(tienVay, laiSuat, soThang);
+        System.out.printf("Tổng số tiền có được khi hết thời hạn cho vay là: %.2f", tongTien);
+
+        sc.close();
+    }
+
+    public static double tinhTienLai(double tienVay, double laiSuat, int soThang) {
+        for (int thang = 1; thang <= soThang; thang++) {
+            double tienLai = tienVay * (laiSuat / 100);
+            tienVay += tienLai;
+        }
+        return tienVay;
     }
 }
